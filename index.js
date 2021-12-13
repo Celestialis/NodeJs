b55a6ec98ff57a1a61ce0a43537dc1dadf7d785f
Celestialis <nagibatorkir@gmail.com>
2021-12-13 11:52:49 +0700

    Урок 3

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-// минуты-часы-число-месяц-год
-let arrDateArgv = process.argv[2].split('-');
-let arrDate = {
-  minute: arrDateArgv[0],
-  hour: arrDateArgv[1],
-  day: arrDateArgv[2],
-  month: arrDateArgv[3],
-  year: arrDateArgv[4],
-};
-const interval = setInterval(() => {
-  const now = new Date() - new Date().getTimezoneOffset() * 60 * 1000;
-  const target = new Date(
-    arrDate['year'],
-    arrDate['month'],
-    arrDate['day'],
-    arrDate['hour'],
-    arrDate['minute'],
-  );
-  const result = new Date(target - now);
-  const year = result.getFullYear() - 1970;
-  const mounth = result.getMonth() - 1;
-  const days = result.getDay();
-  const hours = result.getHours();
-  const minutes = result.getMinutes();
-  const seconds = result.getSeconds();
- 
-  if (result.getTime() - new Date('Sun 1 February 1970 00:00:00').getTime() <= 0) {
-    console.log('Время истекло');
-    clearInterval(interval);
-  } else {
-     console.log(
-    `${year} лет, ${mounth} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`,
-  );
-  }
-}, 1000);
+const fs = require('fs')
+const readline = require('readline')
+
+const readStream = fs.createReadStream('access.log', 'utf8')
+const writeStream1 = fs.createWriteStream('89.123.1.41_requests.log')
+const writeStream2 = fs.createWriteStream('34.48.240.111_requests.log')
+
+let numStr = 0
+
+const rl = readline.createInterface({
+    input: readStream,
+    terminal: true
+});
+
+rl.on('line', (line) => {
+    if (line.includes("89.123.1.41")) {
+        writeStream1.write(line + "\n")
+    }
+
+    if (line.includes("34.48.240.111")) {
+        writeStream2.write(line + "\n")
+    }
+
+    console.log(++numStr)
+})
